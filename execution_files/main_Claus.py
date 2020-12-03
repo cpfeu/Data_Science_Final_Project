3e86dcc63c939c86a871c01bb6a7a0385128a785
Claus Feuring <cpfeuring@gmail.com>
2020-12-03 02:30:36 -0500

    CombinedVisualizer plot functions

--- execution_files/main_Claus.py
+++ execution_files/main_Claus.py
@@ -3,6 +3,7 @@ from data_pulling.stock_pulling import StockPuller
 from configurations.global_config import GlobalConfig
 from data_preprocessing.stock_preprocessing import StockParser
 from visualizations.stock_visualization import StockVisualizer
+from visualizations.social_media_stock_visualization import CombinedVisualizer
 
 
 
@@ -20,18 +21,23 @@ if __name__ == '__main__':
     #stock_puller.pull_data()
 
     # ==================== Parse Stock Data ====================
-    stock_parser = StockParser(ticker=GlobalConfig.TESLA_TICKER_STR, interval=GlobalConfig.ONE_MIN_INTERVAL)
+    stock_parser = StockParser(ticker=GlobalConfig.BANK_OF_AMERICA_TICKER, interval=GlobalConfig.ONE_MIN_INTERVAL)
     rec_list = stock_parser.parse_stock_data()
     stock_feature_dict = stock_parser.create_stock_feature_dict(single_stock_recording_list=rec_list,
                                                                 start_time='09:00:00', end_time='17:00:00',
                                                                 store_in_pkl_file=True)
 
     # ==================== Visualize Stock Data ====================
-    #stock_visualizer = StockVisualizer(single_stock_recording_list=rec_list,
-    #                                   stock_feature_dict=stock_feature_dict)
-    #stock_visualizer.plot_all_in_one_chart()
-    #stock_visualizer.plot_stock_features()
-    #stock_visualizer.plot_stock_feature_histogranms()
+    stock_visualizer = StockVisualizer(single_stock_recording_list=rec_list,
+                                      stock_feature_dict=stock_feature_dict)
+    stock_visualizer.plot_all_in_one_chart()
+    stock_visualizer.plot_stock_features()
+    stock_visualizer.plot_stock_feature_histogranms()
+
+    # ==================== Visualize Stock and Social Media Features ====================
+    combined_visualizer = CombinedVisualizer(stock_ticker=GlobalConfig.BANK_OF_AMERICA_TICKER)
+    combined_visualizer.plot_sentiment_stock_feature_grafics()
+    combined_visualizer.plot_correlation_bar_chart()
 
     # ending time
     ending_time = datetime.now()
